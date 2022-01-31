90a4435bdc23dd90bc9413c105320c83e04739ca
skiyl9x <skiyl9x@gmail.com>
2022-02-01 00:29:38 +0200

    test

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -3,6 +3,16 @@
 #
 #
 
+variable "region" {
+  default     = "us-east-2"
+  description = "AWS region"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+
 resource "random_password" "list" {
   count   = 2
   length  = 16
@@ -15,6 +25,7 @@ resource "aws_secretsmanager_secret" "db_master" {
   name_prefix = "db_master-"
   description = "Master password for MariaDB server"
 }
+
 resource "aws_secretsmanager_secret_version" "db_master_ver" {
   secret_id     = aws_secretsmanager_secret.db_master.id
   secret_string = random_password.list[0].result
@@ -24,6 +35,7 @@ resource "aws_secretsmanager_secret" "db_user" {
   name_prefix = "db_user-"
   description = "User password for MariaDB server"
 }
+
 resource "aws_secretsmanager_secret_version" "db_user_ver" {
   secret_id     = aws_secretsmanager_secret.db_user.id
   secret_string = random_password.list[1].result
