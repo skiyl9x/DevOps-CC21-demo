4243d9cdc3e7179a4b56ed9c91ae522ef24f96ec
skiyl9x <skiyl9x@gmail.com>
2022-02-01 00:14:44 +0200

    test

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -27,9 +27,9 @@ module "vpc" {
   azs                  = data.aws_availability_zones.available.names
   private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
-  enable_dns_hostnames = true
+  enable_nat_gateway   = false
+  single_nat_gateway   = false
+  enable_dns_hostnames = false
 
   tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
