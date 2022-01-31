ffdc4fd194eaa4bcacdc4aa1a64462fbe9a7bafa
skiyl9x <skiyl9x@gmail.com>
2022-02-01 00:24:57 +0200

    test

---
+++ terraform/secrets.tf
@@ -0,0 +1,30 @@
+#Generate secrets for services
+#
+#
+#
+
+resource "random_password" "list" {
+  count   = 2
+  length  = 16
+  special = true
+  override_special = "!@$&"
+}
+
+
+resource "aws_secretsmanager_secret" "db_master" {
+  name_prefix = "db_master-"
+  description = "Master password for MariaDB server"
+}
+resource "aws_secretsmanager_secret_version" "db_master_ver" {
+  secret_id     = aws_secretsmanager_secret.db_master.id
+  secret_string = random_password.list[0].result
+}
+
+resource "aws_secretsmanager_secret" "db_user" {
+  name_prefix = "db_user-"
+  description = "User password for MariaDB server"
+}
+resource "aws_secretsmanager_secret_version" "db_user_ver" {
+  secret_id     = aws_secretsmanager_secret.db_user.id
+  secret_string = random_password.list[1].result
+}
