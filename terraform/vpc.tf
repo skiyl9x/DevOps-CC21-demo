f3983ec9cdeff6fb3608ab0163e9f26ca0547141
skiyl9x <skiyl9x@gmail.com>
2022-02-01 18:48:50 +0200

    change vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,7 +6,6 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.2.0"
 
-
   name                 = "${local.cluster_name}-vpc"
   cidr                 = var.vpc_cidr
   azs                  = data.aws_availability_zones.available.names
@@ -22,11 +21,11 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                    = "1"
+    "kubernetes.io/role/elb"                      = "1"
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"           = "1"
+    "kubernetes.io/role/internal-elb"             = "1"
   }
 }
